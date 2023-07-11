e8fdb00b303f6a8766cd92047134960db4b19358
ewurapoms <apowurah@outlook.com>
2023-07-11 05:37:11 -0700

    code update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-
 def islower(c):
-    return (ord(97) <= ord(c) <= ord(122))
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
+    else:
+        return (False)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-
 def uppercase(str):
     print("{}".format(str.translate(
-        {(c | 32): c for c in range(ord(65), ord(90) + 1)}
+        {(c | 32): c for c in range(ord('A'), ord('Z') + 1)}
     )))
