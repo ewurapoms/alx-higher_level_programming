d713f457c2124e3cb4df10f3d8eccb807e8ce93f
ewurapoms <apowurah@outlook.com>
2023-07-16 02:25:37 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return (None)
-
-    el_mnt = len(my_list)
-
-    if idx > el_mnt - 1:
-        return (None)
-
-    return(my_list[idx])
+    if my_list is not None and -1 < idx < len(my_list):
+        return my_list[idx]
+    return (None)
