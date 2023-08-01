4881dd5a22a4db1284566d32302ed37f91405ecd
ewurapoms <apowurah@outlook.com>
2023-08-01 10:44:49 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Shows a class 'Square' that defines a square"""
+
+
+class Square:
+    """ The empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a Square """
+
+
+class Square():
+    """ Represents a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Creates a 'Square'
+        """
+        self.__size = size
