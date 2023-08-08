1280a08611b2fbb49e2372618b016b8dfeb15568
ewurapoms <apowurah@outlook.com>
2023-08-08 08:29:04 -0700

    pycodestyle update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-""" Declares a locked class """
+"""Declares a locked class"""
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Initializes LockedClass attributes
+    for only attributes declared as 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
