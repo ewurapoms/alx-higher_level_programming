81045fed54828f4e41bcf5192a2639578ae93697
ewurapoms <apowurah@outlook.com>
2023-09-19 05:30:26 -0700

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Lists the cities with the highest average
-SELECT `city`, AVG(`value`) AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `average_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `avg_temp` DESC
+ORDER BY `average_temp` DESC
 LIMIT 3;
