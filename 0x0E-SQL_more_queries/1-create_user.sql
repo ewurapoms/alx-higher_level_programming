4a85c954b64a858f86add46d717664882d89ad32
ewurapoms <apowurah@outlook.com>
2023-09-20 06:41:27 -0700

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates all privileges for user_0d_1
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
