727989df02a557450666ca6923bf0b5e73fda88c
ewurapoms <mzpomaa35@gmail.com>
2023-10-16 15:11:23 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let cpj = 0;
 
-while (cpj <  sentences.length) {
+while (cpj < sentences.length) {
   console.log(sentences[cpj]);
   cpj++;
 }
