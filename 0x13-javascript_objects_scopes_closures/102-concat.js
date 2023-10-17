1a8ee6e1548a24f9b5dec4f55f7f3ce146af0151
ewurapoms <mzpomaa35@gmail.com>
2023-10-17 06:25:54 -0700

    advanced tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const args = require('args');
-const fileA = args.readFileSync(process.argv[2], 'utf8');
-const fileB = args.readFileSync(process.argv[3], 'utf8');
-args.writeFileSync(process.argv[4], fileA + fileB);
+const fs = require('fs');
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], fileA + fileB);
