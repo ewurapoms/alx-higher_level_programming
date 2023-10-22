8d02df60729253ec8fa13fc1ca64fa630dee64cb
ewurapoms <mzpomaa35@gmail.com>
2023-10-21 23:01:14 -0700

    mandatory tasks

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" """
+import sys
+from model_city import City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                            )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
