c7e3a52693fd684d4ece91d82a40adfc1f174f83
ewurapoms <mzpomaa35@gmail.com>
2023-10-22 03:18:51 -0700

    advanced tasks

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
+from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
-from relationship_state import Base, State
 
 
 if __name__ == "__main__":
