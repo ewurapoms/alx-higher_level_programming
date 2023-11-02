4ce16370b06db012cecf4966ded3ef9180d21b4e
ewurapoms <mzpomaa35@gmail.com>
2023-11-02 05:53:31 -0700

    mandatory tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends it a request
-# and prints the size of the body of the response
-curl -s "$1" | wc -c
+# Bash script that takes in and sends request to URL 
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
