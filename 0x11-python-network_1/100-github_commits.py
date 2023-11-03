99003c6ad376fb4ee60a8369e45b085bc78583e7
ewurapoms <mzpomaa35@gmail.com>
2023-11-03 06:41:32 -0700

    advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 
+script lists 10 most recent commits 
+data from a GitHub repository
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+
+    author = sys.argv[1]
+    repo = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(author,
+                                                                repo)
+
+    data = requests.get(url)
+    commits = data.json()
+
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
